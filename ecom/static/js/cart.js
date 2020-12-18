f9913473e59b3247f84598bae916d2f27165dbe1
rathod-aryan <75719450+rathod-aryan@users.noreply.github.com>
2020-12-18 19:21:53 +0530

    add to cart linked

--- ecom/static/js/cart.js
+++ ecom/static/js/cart.js
@@ -5,6 +5,39 @@ for(i=0; i<n.length; i++){
         var pname = this.dataset.product
         var action = this.dataset.action
         console.log('product name:', pname,'action:',action)
+        console.log('product Name:', pname,'action:',action)
+
+        console.log('USER:', user)
+        if(user == 'AnonymousUser'){
+            console.log('no you')
+        }
+        else{
+            updateUserOrder(pname,action)
+        }
+
+        
     })
 
+}
+
+
+function updateUserOrder(pname,action){
+    console.log('yess yess hmm yess')
+
+    var url = '/update_item/'
+    fetch(url,{
+        method : 'POST',
+        headers:{ 'Content-Type': 'application/json',
+                  'X-CSRFToken' : csrftoken,
+    },
+        body:JSON.stringify({'pname': pname,'action': action})
+    })
+
+    .then((response) => {
+        return response.json()
+    })
+
+    .then((data) => {
+        console.log('data:',data)
+    })
 }
--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
+import json
+
 
 def store(request):
 	if request.user.is_authenticated:
@@ -32,3 +35,28 @@ def checkout(request):
 		order = {'get_cart_total':0, 'get_cart_items':0}
 	context = {'items':items, 'order':order}
 	return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+	data = json.loads(request.body)
+	pname= data['pname']
+	action = data['action']
+	print('Action:',action)
+	print('Product Name:',pname)
+
+	customer = request.user.customer
+	product = Product.objects.get(name=pname)
+	order, created = Order.objects.get_or_create(customer = customer, complete = False)
+	
+	
+	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action== 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
+
+	orderItem.save()
+
+	if orderItem.quantity <= 0 :
+		orderItem.delete()
+	return JsonResponse('Item was added',safe=False)
