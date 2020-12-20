351b49dc96a4b150fc78752ef932c15e1beca218
ritik9294 <75716583+ritik9294@users.noreply.github.com>
2020-12-20 22:07:41 +0530

    Update urls.py

--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -11,4 +11,11 @@ urlpatterns = [
 	path('signin/', views.signin, name="signin"),
 	path('signout/', views.signout, name="signout"),
 	path('signup/', views.signup, name="signup"),
+	path('reset_password/', auth_views.PasswordResetView.as_view(template_name="store/password_reset.html") , name="reset_password"),
+
+	path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="store/password_reset_sent.html") , name="password_reset_done"),
+
+	path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="store/password_reset_form.html") , name="password_reset_confirm"),
+
+	path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(template_name="store/password_reset_done.html") , name="password_reset_complete"),
 ]
