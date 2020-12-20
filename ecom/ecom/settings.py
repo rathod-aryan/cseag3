a1a6f18c82d340e67a71223a33b55c7f88f35470
rathod-aryan <75719450+rathod-aryan@users.noreply.github.com>
2020-12-20 23:47:27 +0530

    Preliminary Final Commit

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -135,5 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dashingritik123@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ritik@123'
+EMAIL_HOST_USER = 'testuser.cseaecom@gmail.com'
+EMAIL_HOST_PASSWORD = 'csea2021g3'
